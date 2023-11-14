78b4dcf249b186eb5ac0dbd3e70fd1f17df8a3ab
Milena Oliveira <milenaoliveira.ea@gmail.com>
2023-11-14 16:48:15 -0300

    primeiro commit

---
+++ dia_01.js
@@ -0,0 +1,7 @@
+alert('hello word');
+alert('convensorDeMoedas');
+var valorEmDolar =30;
+var cotacaoDoDolar = 5.32;
+var valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal= valorEmReal.toFixed(2);
+alert ('R$' + valorEmReal)
